bd3896434ed1c68e755bb0e178721124bcf63ae1
Markus Krogemann <markus@krogemann.de>
2014-05-16 10:00:08 +0200

    java skeleton

---
+++ here2014/java/src/test/java/conway/DummyTest.java
@@ -0,0 +1,13 @@
+package conway;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyTest {
+
+    @Test
+    public void truth() throws Exception {
+        assertEquals("true should be true", true, false);
+    }
+}
