baf2a0109307f85340d9dba627f574298c85aac7
Markus Krogemann <markus@krogemann.de>
2014-05-16 14:59:28 +0200

    ruby skeleton

---
+++ here2014/ruby/lib/dummy.rb
@@ -0,0 +1,7 @@
+class Dummy
+
+  def say_something
+    puts "something"
+  end
+
+end
---
+++ here2014/ruby/spec/dummy_spec.rb
@@ -0,0 +1,5 @@
+describe 'The Truth' do
+  it 'is the truth, nothing but the truth' do
+    true.should be_true
+  end
+end
